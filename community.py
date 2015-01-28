8e837167f00e130b3bb2c20d2afd58d15a215dbb
David Reeve <nerdboy6@gmail.com>
2015-01-28 14:09:32 -0500

    Added some comments
    Not a lot of them but you get the point

--- community.py
+++ community.py
@@ -2,18 +2,16 @@
 # Author:       Lancey
 # Description:  Provides wrappers for interfacing with the Steam Community
 # Last Update:  1/28/2015
-from bs4 import BeautifulSoup
-import pickle
-import datetime
-import os
-import random
-import re
-import urllib
-import urllib2
+from bs4 import BeautifulSoup # BeautifulSoup for XML parsing (probably overkill)
+import cPickle as pickle      # pickle for saving data to our cache
+import datetime               # datetime for getting the current year and finding cache expiration times
+import os                     # os interface for checking if the cache already exists
+import re                     # regex for regex
+import urllib2                # url access interface
 
-COMMUNITY_CACHE_FILE  = "community_data.cache"
-CACHE_EXPIRE_TIME     = 900
-EXPANDED_MONTHS       = { "Jan" : "January",
+COMMUNITY_CACHE_FILE  = "community_data.cache"  # filename to cache the community data to
+CACHE_EXPIRE_TIME     = 900                     # time between cache creation and expiration (15 minutes)
+EXPANDED_MONTHS       = { "Jan" : "January",    # expanded form of 3 letter months
                           "Feb" : "February",
                           "Mar" : "March",
                           "Apr" : "April",
@@ -26,10 +24,12 @@ EXPANDED_MONTHS       = { "Jan" : "January",
                           "Nov" : "November",
                           "Dec" : "December" }
 
+# gets the modification time of a file as datetime
 def modification_time( filename ) :
   t = os.path.getmtime( filename )
   return datetime.datetime.fromtimestamp( t )
 
+# gets the group info dict from the cache or requests a new dict
 def get_group_info( group ) :
   if not os.path.exists( COMMUNITY_CACHE_FILE ) :
     request_group_info( group )
@@ -47,6 +47,7 @@ def get_group_info( group ) :
       except IOError :
         return request_group_info( group )
 
+# requests a new copy of the group events and announcements from steam
 def request_group_info( group ) :
   with open( COMMUNITY_CACHE_FILE, 'wb' ) as f :
     data = {
@@ -73,15 +74,19 @@ def _remove_community_linkfilter( s ) :
   return s.replace( "https://steamcommunity.com/linkfilter/?url=", "" )
  
 class SteamGroup :
+  # make a group content request to steam, getting only the necessary xml back
   def _requestGroupContent( self, fn, module, id=None ) :
     if id :
       url = self.groupUrl + '/' + module + '/' + str( id ) + "?content_only=true"
     else :
       url = self.groupUrl + '/' + module + "?content_only=true"
+
+    # construct and send a request
     req = urllib2.Request( url )
     f   = urllib2.urlopen( req )
     return fn( f )
 
+  # reads through an xml response and builds a dict of data to construct a new event
   @staticmethod
   def _parseEventDetails( req ) :
     bsdata = BeautifulSoup( _remove_community_linkfilter( req.read( ) ) )
@@ -93,12 +98,14 @@ class SteamGroup :
     }
     return data
 
+  # reads through an xml response and builds a list of all event ids
   @staticmethod
   def _parseEventList( req ) :
     pattern = "(\d+)_eventBlock"
     bsdata = BeautifulSoup( req.read() )
     return [re.search( pattern, e['id'] ).group( 1 ) for e in bsdata.find_all( "div", class_="eventBlock" )]
 
+  # reads through an xml response and builds a dict of data to construct a new announcement
   @staticmethod
   def _parseAnnouncementDetails( req ) :
     bsdata = BeautifulSoup( _remove_community_linkfilter( req.read( ) ) )
@@ -109,26 +116,31 @@ class SteamGroup :
     }
     return data
 
+  # reads through an xml response and build a list of all announcement ids
   @staticmethod
   def _parseAnnouncementList( req ) :
     pattern = "abody_(\d+)"
     bsdata = BeautifulSoup( req.read() )
     return [re.search( pattern, e['id'] ).group( 1 ) for e in bsdata.find_all( "div", class_="bodytext" )]
 
+  # gets the details of a particular event by event id
   def getEventDetails( self, id ) :
     data = self._requestGroupContent( self._parseEventDetails, "events", id )
     return Event( data['title'], data['date'], data['headline'], data['desc'], self.groupUrl + '/events/' + id )
 
+  # gets a list of all event ids this month
   def getEventList( self ) :
     data = []
     for eid in self._requestGroupContent( self._parseEventList, "events" ) :
       data.append( self.getEventDetails( eid ) )
     return data
 
+  # gets the details of a particular announcement by id
   def getAnnouncementDetails( self, id ) :
     data = self._requestGroupContent( self._parseAnnouncementDetails, "announcements/detail", id )
     return Announcement( data['title'], data['date'], data['desc'], self.groupUrl + "/announcements/detail/" + id )
 
+  # gets a list of the past 5 announcement ids
   def getAnnouncementList( self ) :
     data = []
     for aid in self._requestGroupContent( self._parseAnnouncementList, "announcements" ) :
