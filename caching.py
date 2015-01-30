534970e14b3d3ced281d0dd1e31c5b3ed0db408e
David Reeve <nerdboy6@gmail.com>
2015-01-30 09:17:02 -0500

    Added logging messages
    caching.py now sends debug messages to the logger

--- caching.py
+++ caching.py
@@ -20,6 +20,7 @@ class Cache :
     # gets the last modification time of the cache
     def get_modification_time( self ) :
       if not self.exists( ) :
+        self.logger.warning( "caching %s : %s", self.filename, "get_modification_time called on nonexistant cache" )
         return datetime.datetime.min
       return datetime.datetime.fromtimestamp( os.path.getmtime( self.filename ) )
 
@@ -41,6 +42,7 @@ class Cache :
     # gets the cache data and updates the cache if it needs to
     def get( self ) :
       if not self.exists( ) :
+        self.logger.info( "caching %s : %s", self.filename, "creating new cache" )
         return self.request_and_update( )
 
       difftime = self.get_time_since_modified( )
@@ -52,10 +54,12 @@ class Cache :
           with self.open( 'rb' ) as f :
             data = pickle.load( f )
             if not self.valid( data ) :
+              self.logger.info( "caching %s : %s", self.filename, "cache is invalid, rebuilding it" )
               return self.request_and_update( )
             else :
               return data
         except (IOError, EOFError) :
+          self.logger.error( "caching %s : %s", self.filename, "error encountered while reading cache" )
           return self.request_and_update( )
 
     # opens the cache file and returns the file handler
@@ -85,3 +89,5 @@ class Cache :
 
       self.request  = requestfn
       self.valid    = validatefn
+
+      self.logger   = logging.getLogger( 'caching' )
